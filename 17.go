27babf90db42f6d1fd03d5e744234b2763c43ed7
Madhurjya Deep Chakravarty <madhurjyadeepch@gmail.com>
2026-08-20 23:53:11 +0530

    Add exercises upto defer

---
+++ 17.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Counting...")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("Done")
+}
+
+// Outcome: 
+
+// Counting...
+// Done...
+
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0 
